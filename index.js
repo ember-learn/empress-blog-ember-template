39a96ec2229fc7aaa09a8a58149d48433f0ce132
Chris Manson <chris.manson@simplabs.com>
2018-11-06 18:17:10 +0000

    removing unused packages

--- index.js
+++ index.js
@@ -1,45 +1,5 @@
 'use strict';
 
-const autoprefixer = require('autoprefixer');
-const colorFunction = require('postcss-color-function');
-const cssnano = require('cssnano');
-const customProperties = require('postcss-custom-properties');
-const postcssImport = require('postcss-import');
-
 module.exports = {
-  name: 'ember-ghost-casper-template',
-
-  options: {
-    postcssOptions: {
-      compile: {
-        enable: true,
-        plugins: [
-          { module: postcssImport },
-          { module: customProperties },
-          { module: colorFunction },
-          { module: autoprefixer },
-          { module: cssnano },
-        ]
-      }
-    }
-  },
-
-  included(app) {
-    this._super.included.apply(this, arguments)
-
-    app.options.postcssOptions = Object.assign({
-        compile: {
-          enable: true,
-          plugins: [
-            { module: postcssImport },
-            { module: customProperties },
-            { module: colorFunction },
-            { module: autoprefixer },
-            { module: cssnano },
-          ]
-        }
-      },
-      app.options.postcssOptions
-    );
-  },
+  name: 'ember-ghost-ember-template',
 };
