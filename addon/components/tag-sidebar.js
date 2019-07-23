37ee2da3dc3e99b4122ecacb29069efc9ff5218d
Amy Lam <amy.lam@betterup.co>
2019-07-23 09:11:31 -0700

    WIP tag work
    Co-authored-by: Chris Manson <chris@manson.ie>

--- addon/components/tag-sidebar.js
+++ addon/components/tag-sidebar.js
@@ -2,9 +2,35 @@ import Component from '@ember/component';
 import layout from '../templates/components/tag-sidebar';
 import { inject as service } from '@ember/service';
 import { computed } from '@ember/object';
+import { filter } from '@ember/object/computed';
 
 export default Component.extend({
   router: service(),
+
+  fiteredTags: filter('tags.[]', ['yearTags'], function(tag) {
+    let individualIgnoredTags = [
+      'releases',
+      'new',
+      'newsletter',
+    ];
+
+    let allIgnoredTags = [
+      ...individualIgnoredTags,
+      ...this.yearTags.map(tag => tag.id),
+      ...this.versionTags.map(tag => tag.id),
+    ]
+
+    return !allIgnoredTags.includes(tag.id);
+  }),
+
+  yearTags: filter('tags.[]', function(tag) {
+    console.log(tag.id, tag.id.match(/^20\d\d$/))
+    return tag.id.match(/^20\d\d$/);
+  }),
+
+  versionTags: filter('tags.[]', function(chore, index, array) {
+    
+  }),
 
   showSidebar: computed('router.currentRouteName', function() {
     if (this.router.currentRouteName === 'author') {
