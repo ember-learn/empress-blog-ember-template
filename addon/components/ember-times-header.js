5b68b01827a544c8ab27eedc9268e6d217795607
Chris Manson <chris@manson.ie>
2020-03-26 11:22:07 +0000

    convert ember-times header

--- addon/components/ember-times-header.js
+++ addon/components/ember-times-header.js
@@ -1,11 +1,7 @@
-import Component from '@ember/component';
-import { computed } from '@ember/object';
-import layout from '../templates/components/ember-times-header';
+import Component from '@glimmer/component';
 
-export default Component.extend({
-  layout,
-
-  isNewsletter: computed('tags.[]', function() {
-    return this.tags && this.tags.includes('Newsletter');
-  })
-});
+export default class TimesHeaderComponent extends Component {
+  get isNewsletter() {
+    return this.args.tags && this.args.tags.includes('Newsletter');
+  }
+}
