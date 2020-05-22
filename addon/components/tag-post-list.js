37f3e547132e0e80286dddd3165a406bf11f263f
Chris Manson <chris@manson.ie>
2020-05-22 23:33:59 +0100

    make it so that we only async load the first 3 posts in a tag

--- addon/components/tag-post-list.js
+++ addon/components/tag-post-list.js
@@ -1,4 +1,8 @@
 import Component from '@glimmer/component';
+import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
+
+import DS from 'ember-data';
 
 const topPostCount = 3;
 
@@ -9,8 +13,22 @@ const topPostCount = 3;
  *   <TagPostList @tag={{tag}} />
  */
 export default class TagPostList extends Component {
-  get topPosts() {
-    return this.args.tag.posts.slice(0, topPostCount);
+  @service store;
+
+  // this can't be a getter because returning a `DS.PromiseArray` from a getter
+  // causes an infinte loop
+  @tracked topPosts;
+
+  constructor() {
+    super(...arguments);
+
+    let ids = this.args.tag.hasMany('posts').ids().slice(0, topPostCount);
+
+    let promise = Promise.all(ids.map(id => this.store.findRecord('content', id)));
+
+    this.topPosts = DS.PromiseArray.create({
+      promise,
+    });
   }
 
   get titleId() {
