9d415c80697d26e5a275103247addec49e146a1e
Nick Schot <nickschot@gmail.com>
2020-07-09 12:33:15 +0200

    make loading of posts more robust

--- addon/components/load-posts.js
+++ addon/components/load-posts.js
@@ -2,6 +2,7 @@ import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
+import { task } from 'ember-concurrency';
 
 export default class LoadPostsComponent extends Component {
   @service store;
@@ -29,10 +30,15 @@ export default class LoadPostsComponent extends Component {
 
   @action
   async loadInitial(model) {
-    this.posts = [];
-    this.page = 1;
-    this.posts = (await model.posts).sortBy('date').reverse();
+    this.loadInitialTask.perform(model);
   }
+
+  @(task(function*(model) {
+    this.page = 1;
+    let posts = yield model.posts;
+    this.posts = posts ? posts.sortBy('date').reverse() : [];
+  }).restartable())
+  loadInitialTask;
 
   @action
   loadMore() {
