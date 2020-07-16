65f6e2502bdbadeff550e77cb46ce08384ac3f47
Nick Schot <nickschot@gmail.com>
2020-07-16 15:28:52 +0200

    close sidebar after a route transition

--- addon/components/sidebar.js
+++ addon/components/sidebar.js
@@ -1,9 +1,24 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default class SidebarComponent extends Component {
+  @service router;
+
   @tracked isOpen = false;
+
+  constructor() {
+    super(...arguments);
+
+    this.router.on('routeDidChange', this.close);
+  }
+
+  willDestroy() {
+    this.router.off('routeDidChange', this.close);
+
+    super.willDestroy(...arguments);
+  }
 
   @action
   toggle() {
@@ -12,6 +27,8 @@ export default class SidebarComponent extends Component {
 
   @action
   close() {
-    this.isOpen = false;
+    if (this.isOpen) {
+      this.isOpen = false;
+    }
   }
 }
