86b68b68364e15a6207df2a778133f8a33ce6300
Ignace Maes <ignacemaes100@msn.com>
2023-09-11 14:21:18 +0200

    include version-5 in tags

--- addon/components/tag-panel.js
+++ addon/components/tag-panel.js
@@ -4,7 +4,7 @@ import Component from '@glimmer/component';
 const recentPostsTagId = 'new';
 const releasesTagId = 'releases';
 const emberTimesTagId = 'newsletter';
-const versionTagIds = ['version-4-x', 'version-3-x', 'version-2-x', 'version-1-x'];
+const versionTagIds = ['version-5-x', 'version-4-x', 'version-3-x', 'version-2-x', 'version-1-x'];
 const yearTagIds = ['2025', '2024', '2023', '2022', '2021', '2020', '2019', '2018', '2017', '2016', '2015', '2014', '2013', '2012'];
 
 /**
