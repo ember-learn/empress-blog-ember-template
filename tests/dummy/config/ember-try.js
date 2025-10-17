e81706f9ba29174bd688c78d0f69a68a6dd3f997
Chris Manson <chris@manson.ie>
2025-10-17 16:24:06 +0100

    update to v4.12 with ember-cli-update
    # Conflicts:
    #	.gitignore
    #	.npmignore
    #	README.md
    #	ember-cli-build.js
    #	tests/dummy/app/index.html
    #	tests/dummy/config/ember-try.js
    #	tests/dummy/config/environment.js

--- tests/dummy/config/ember-try.js
+++ tests/dummy/config/ember-try.js
@@ -27,7 +27,7 @@ module.exports = async function () {
         name: 'ember-lts-3.24',
         npm: {
           devDependencies: {
-            'ember-source': '~3.24.3',
+            'ember-source': '~3.24.0',
           },
         },
       },
@@ -40,10 +40,25 @@ module.exports = async function () {
         },
       },
       {
+        name: 'ember-lts-4.4',
+        npm: {
+          devDependencies: {
+            'ember-source': '~4.4.0',
+          },
+        },
+      },
+      {
+        name: 'ember-lts-4.8',
+        npm: {
+          devDependencies: {
+            'ember-source': '~4.8.0',
+          },
+        },
+      },
+      {
         name: 'ember-release',
         npm: {
           devDependencies: {
-            'ember-auto-import': '^2.0.0',
             'ember-source': await getChannelURL('release'),
           },
         },
@@ -52,7 +67,6 @@ module.exports = async function () {
         name: 'ember-beta',
         npm: {
           devDependencies: {
-            'ember-auto-import': '^2.0.0',
             'ember-source': await getChannelURL('beta'),
           },
         },
@@ -61,56 +75,12 @@ module.exports = async function () {
         name: 'ember-canary',
         npm: {
           devDependencies: {
-            'ember-auto-import': '^2.0.0',
             'ember-source': await getChannelURL('canary'),
           },
         },
       },
-      {
-        name: 'ember-default-with-jquery',
-        env: {
-          EMBER_OPTIONAL_FEATURES: JSON.stringify({
-            'jquery-integration': true,
-          }),
-        },
-        npm: {
-          devDependencies: {
-            '@ember/jquery': '^1.1.0',
-          },
-        },
-      },
-      {
-        name: 'ember-classic',
-        env: {
-          EMBER_OPTIONAL_FEATURES: JSON.stringify({
-            'application-template-wrapper': true,
-            'default-async-observers': false,
-            'template-only-glimmer-components': false,
-          }),
-        },
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.28.0',
-          },
-          ember: {
-            edition: 'classic',
-          },
-        },
-      },
-      embroiderSafe({
-        npm: {
-          devDependencies: {
-            'ember-auto-import': '^2.0.0',
-          },
-        },
-      }),
-      embroiderOptimized({
-        npm: {
-          devDependencies: {
-            'ember-auto-import': '^2.0.0',
-          },
-        },
-      }),
+      embroiderSafe(),
+      embroiderOptimized(),
       {
         name: 'no-deprecations',
         npm: {
